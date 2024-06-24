6ca840d767e231f9ad0a3c5ff0c63bdc10e09b0f
爱喝水的木子 <50564847+ice-a@users.noreply.github.com>
2024-06-24 19:14:33 +0800

    Update FaceGenerator.vue

--- src/views/FaceGenerator.vue
+++ src/views/FaceGenerator.vue
@@ -1,13 +1,5 @@
 <template>
-  <div><p><a href="http://qd.lideshan.top/my/" target="_blank">自动化签到</a></p>
-    <p><a href="http://pan.lideshan.top/" target="_blank">木子的秘密</a></p>
-    <p><a href="https://cook.lideshan.top/" target="_blank">一位大厨的崛起之路</a></p>
-    <p><a href="https://tools.lideshan.top/" target="_blank">木子的工具箱</a></p>
-    <p><a href="https://eat.lideshan.top/" target="_blank">不知道吃什么就去看看</a></p>
-    <p><a href="https://remake.lideshan.top/public/index.html" target="_blank">重生之我在人间两万年</a></p>
-    <p><a href="https://ip.lideshan.top/" target="_blank">木子的ip信息,爆破他</a></p>
-    <p><a href="https://lideshan.top/" target="_blank">去访问有趣的木子</a></p>
-    <p>Powered by Cloudflare Workers</p></div>
+
   <div class="container">
     <svg
       viewBox="-100 -100 200 200"
@@ -278,7 +270,17 @@
     </svg>
     <button @click="generateFace">ANOTHER</button>
     <button @click="downloadSVGAsPNG">DOWNLOAD</button>
+    <div><p><a href="http://qd.lideshan.top/my/" target="_blank">自动化签到</a></p>
+    <p><a href="http://pan.lideshan.top/" target="_blank">木子的秘密</a></p>
+    <p><a href="https://cook.lideshan.top/" target="_blank">一位大厨的崛起之路</a></p>
+    <p><a href="https://tools.lideshan.top/" target="_blank">木子的工具箱</a></p>
+    <p><a href="https://eat.lideshan.top/" target="_blank">不知道吃什么就去看看</a></p>
+    <p><a href="https://remake.lideshan.top/public/index.html" target="_blank">重生之我在人间两万年</a></p>
+    <p><a href="https://ip.lideshan.top/" target="_blank">木子的ip信息,爆破他</a></p>
+    <p><a href="https://lideshan.top/" target="_blank">去访问有趣的木子</a></p>
+    <p>Powered by Cloudflare Workers</p></div>
   </div>
+
 </template>
 
 <script>
